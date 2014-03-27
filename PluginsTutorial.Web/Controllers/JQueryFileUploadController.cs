165892dc179dd276ca5f268a8b4bbc1355ddb8de
Taraman <taraman@invivolink.com>
2014-03-27 15:32:49 +0200

    add the original version of jquey File Upload

---
+++ PluginsTutorial.Web/Controllers/JQueryFileUploadController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PluginsTutorial.Web.Controllers
+{
+	public class JQueryFileUploadController : Controller
+	{
+
+		public ActionResult Basic()
+		{
+			return View("~/Views/FileUpload/jQueryFileUpload/Basic.cshtml");
+		}
+
+
+
+
+	}
+}
